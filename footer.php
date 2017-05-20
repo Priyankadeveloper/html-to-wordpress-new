10cf55ae0703027ea98abc74b2b14b8acbc719d7
Priyankadeveloper <priyanka08_pal@rediffmail.com>
2017-05-20 04:15:21 -0700

    footer.php
    footer.php

---
+++ footer.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+?>
+
+		</div><!-- .site-content -->
+
+		 <footer id="footer" class="footer">
+      <div class="container text-center">
+    
+     
+      <ul class="social-links">
+        <li><a href="#link"><i class="fa fa-twitter fa-fw"></i></a></li>
+        <li><a href="#link"><i class="fa fa-facebook fa-fw"></i></a></li>
+        <li><a href="#link"><i class="fa fa-google-plus fa-fw"></i></a></li>
+        <li><a href="#link"><i class="fa fa-dribbble fa-fw"></i></a></li>
+        <li><a href="#link"><i class="fa fa-linkedin fa-fw"></i></a></li>
+      </ul>
+        ©2017 Email Marketing. All rights reserved
+        <div class="credits">
+            <!-- 
+                All the links in the footer should remain intact. 
+                You can delete the links only if you purchased the pro version.
+                Licensing information: https://bootstrapmade.com/license/
+                Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Mentor
+            -->
+            Designed by <a href="#">Shrinkcom Software</a>
+        </div>
+      </div>
+    </footer>
+    <!--/ Footer-->
+  
+    <script src="js/bootstrap.min.js"></script>
+ 
+    
+ 
+
+<?php wp_footer(); ?>
+</body>
+</html>
